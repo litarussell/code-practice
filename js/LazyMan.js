1517bec81eb0859cac2fa9e942fead9ffde49a67
tao.li <litarussell@qq.com>
2020-11-27 17:45:42 +0800

    lazyman

---
+++ js/LazyMan.js
@@ -0,0 +1,60 @@
+class _LazyMan {
+    constructor(name) {
+        this.taskQueue = [];
+        this.runTimer = null;
+        this.sayHi(name);
+    }
+
+    run() {
+        if (this.runTimer) {
+            clearTimeout(this.runTimer);
+        }
+
+        this.runTimer = setTimeout(async () => {
+            for (let asyncFun of this.taskQueue) {
+                await asyncFun()
+            }
+            this.taskQueue.length = 0;
+            this.runTimer = null;
+        })
+        return this;
+    }
+
+    sayHi(name) {
+        this.taskQueue.push(async () => console.log(`Hi, this is ${name}`));
+        return this.run();
+    }
+
+    eat(food) {
+        this.taskQueue.push(async () => console.log(`Eat ${food}`));
+        return this.run();
+    }
+
+    sleep(second) {
+        this.taskQueue.push(async () => {
+            console.log(`Sleep ${second} s`)
+            return this._timeout(second)
+        });
+        return this.run();
+    }
+
+    sleepFirst(second) {
+        this.taskQueue.unshift(async () => {
+            console.log(`Sleep first ${second} s`)
+            return this._timeout(second);
+        });
+        return this.run();
+    }
+
+    async _timeout(second) {
+        await new Promise(resolve => {
+            setTimeout(resolve, second * 1e3);
+        })
+    }
+}
+
+// 测试
+var lazyMan = name => new _LazyMan(name)
+
+// lazyMan('Hank');
+lazyMan('Hank').sleep(10).eat('dinner');
